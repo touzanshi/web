7915c5697327ee6a2c8e9e9d1d85bea878f87456
touzhanshi <touzhanshi@qq.com>
2023-02-18 15:30:46 +0800

    upgrade

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from django.utils import timezone
+from .models import Question
+import datetime
 
+
+class QuestionModelTests(TestCase):
+    def test_was_published_recently_with_future(self):
+        time = timezone.now() + datetime.timedelta(days=30)
+        question = Question(pub_date=time)
+        self.assertIs(question.was_published_recently(), False)
+
+    def test_was_published_recently_with_old(self):
+        time = timezone.now() + datetime.timedelta(days=1, seconds=1)
+        question = Question(pub_date=time)
+        self.assertIs(question.was_published_recently(), False)
+
+    def test_was_published_recently_with_recent(self):
+        time = timezone.now() + datetime.timedelta(hours=23, minutes=59, seconds=59)
+        question = Question(pub_date=time)
+        self.assertIs(question.was_published_recently(), False)
 # Create your tests here.
--- polls/views.py
+++ polls/views.py
@@ -1,27 +1,61 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import Http404
-from django.http import HttpResponse
-from .models import Question
+from django.http import HttpResponseRedirect
+from .models import Question, Choice
+from django.urls import reverse
+from django.db.models import F
+from django.views import generic
+from django.utils import timezone
 
 
-def index(request):
-    latest_qustion_list = Question.objects.order_by('pub_date')[:5]
-    template = 'polls/index.html'
-    context = {'latest_question_list': latest_qustion_list}
-    return render(request, template, context)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+
+    # context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('pub_date')
 
 
-def detail(request, question_id):
-    template = 'polls/detail.html'
-    question = get_object_or_404(Question,pk=question_id)
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
 
-    return render(request, template, context={'question': question})
-
-
-def results(request, question_id):
-    response = 'looking at results of question %s'
-    return HttpResponse(response % question_id)
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+
+# def index(request):
+#     latest_question_list = Question.objects.order_by('pub_date')[:5]
+#     template = 'polls/index.html'
+#     context = {'latest_question_list': latest_question_list}
+#     return render(request, template, context)
+#
+#
+# def detail(request, question_id):
+#     template = 'polls/detail.html'
+#     question = get_object_or_404(Question, pk=question_id)
+#
+#     return render(request, template, context={'question': question})
+#
+#
 def vote(request, question_id):
-    return HttpResponse('voting on question %s' % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except(KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html',
+                      {'question': question, 'error_message': "you didn't select a choice."})
+    else:
+        selected_choice.votes = F('votes') + 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+#
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/results.html', {'question': question})
